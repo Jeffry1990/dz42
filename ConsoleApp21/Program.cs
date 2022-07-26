725ebcf187d1e9c7bbd6473de7a389b701353cc7
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2022-07-26 20:14:02 +0300

    Add files via upload

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomMinNumber = 1;
+            int randomMaxNumber = 100;
+            int degree = 1;
+            int baseOfDegree = 2;
+            int finalNumber = 1;
+            Random random = new Random();
+            int number = random.Next(randomMinNumber, randomMaxNumber);
+
+            Console.WriteLine("Случайное число - " + number);
+            
+            int currentNumber;
+            for (currentNumber = baseOfDegree; currentNumber <= number; currentNumber *= baseOfDegree)
+            {
+                degree++;
+            }  
+            Console.WriteLine("Нужная степень двойки - " + degree);
+            Console.WriteLine("число два в найденной степени - " + currentNumber*finalNumber);
+        }
+    }
+}
